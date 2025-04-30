29a418cb0567c6685a34be2afc5006fa9683f66e
Devika-Talwar <67099452+Devika-Talwar@users.noreply.github.com>
2025-04-30 11:16:47 +0200

    normalized.R

--- normalized.R
+++ normalized.R
@@ -1,5 +1,5 @@
 library(DESeq2)
-featurecounts_data <- read.table("/groups/ma/sradata/featurecounts_output/gene_counts.txt", header = TRUE, row.names = 1, sep = "\t")
+featurecounts_data <- read.table("gene_counts.txt", header = TRUE, row.names = 1, sep = "\t")
 head(featurecounts_data)
 count_data <- featurecounts_data[, 7:ncol(featurecounts_data)]
 head(count_data)
@@ -13,7 +13,7 @@ cat("Number of genes after filtering: ", nrow(filtered_data), "\n")
 boxplot(log(filtered_data + 1), main="Boxplot of filtered count data (log-transformed)")
 write.table(filtered_data, "filtered_featurecounts_output.txt", sep = "\t", quote = FALSE)
 getwd()
-write.table(filtered_data, "/groups/ma/sradata/featurecounts_output/filtered_featurecounts_output.txt", sep = "\t", quote = FALSE)
+write.table(filtered_data, "filtered_featurecounts_output.txt", sep = "\t", quote = FALSE)
 samples <- c("Sample1_Replicate1", "Sample1_Replicate2", 
              "Sample2_Replicate1", "Sample2_Replicate2", 
              "Sample3_Replicate1", "Sample3_Replicate2", 
@@ -32,14 +32,10 @@ replicates <- c(1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2)
 colData <- data.frame(row.names = samples, condition = conditions, replicate = replicates  )
 print(colData)
 dds <- DESeqDataSetFromMatrix(countData = countData, colData = coldata, design = ~ condition)
-count_data <- read.table("/groups/ma/sradata/featurecounts_output/filtered_featurecounts_output.txt", header = TRUE, row.names = 1, sep = "\t")
+count_data <- read.table("filtered_featurecounts_output.txt", header = TRUE, row.names = 1, sep = "\t")
 dds <- DESeqDataSetFromMatrix(countData = count_data, colData = colData, design = ~ condition)
 colnames(count_data)
 rownames(colData)
-
-colnames(count_data)
-rownames(colData)
-
 colData$condition <- as.factor(colData$condition)
 str(colData$condition)
 dds <- DESeqDataSetFromMatrix(countData = count_data, 
@@ -48,7 +44,7 @@ dds <- DESeqDataSetFromMatrix(countData = count_data,
 levels(colData$condition)
 dds <- DESeq(dds)
 normalized_counts <- counts(dds, normalized = TRUE)
-write.table(normalized_counts, file = "/groups/ma/sradata/featurecounts_output/normalized_counts.txt", sep = "\t", quote = FALSE)
+write.table(normalized_counts, file = "normalized_counts.txt", sep = "\t", quote = FALSE)
 rlog_counts <- rlog(dds)
 plotPCA(rlog_counts, intgroup = "condition")
 boxplot(assay(rlog_counts), las = 2, main = "Boxplot of Normalized Counts")
